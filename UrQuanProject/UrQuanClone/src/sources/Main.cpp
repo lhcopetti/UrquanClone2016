cfa7b14722b9c7c1e63d224356fa7411f1f876bb
Luis Copetti <lhcopetti@gmail.com>
2016-09-11 11:22:25 -0300

    INT: First commit

---
+++ UrQuanProject/UrQuanClone/src/sources/Main.cpp
@@ -0,0 +1,38 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 4 de set de 2016
+ *      Author: Pichau
+ */
+
+
+
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+
+int main()
+{
+	std::cout << "This is the beginning! " << std::endl;
+
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
