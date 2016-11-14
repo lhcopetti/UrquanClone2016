0e302d70a89cf981b8ccdc587c1f85c29ca7a286
Luis Copetti <lhcopetti@gmail.com>
2016-11-13 22:32:33 -0200

    INT: The ships are shooting each other

--- UrQuanProject/UrQuanClone/src/sources/GameMachine/GameObjects/Armory/ProjectileFactory/SquareBulletFactory.cpp
+++ UrQuanProject/UrQuanClone/src/sources/GameMachine/GameObjects/Armory/ProjectileFactory/SquareBulletFactory.cpp
@@ -56,6 +56,8 @@ Projectile* SquareBulletFactory::createNew()
 	category.add(Collision::CC_WALL);
 	category.add(Collision::CC_PROJECTILE_PLAYERONE);
 	category.add(Collision::CC_PROJECTILE_PLAYERTWO);
+	category.add(Collision::CC_SHIP_PLAYERONE);
+	category.add(Collision::CC_SHIP_PLAYERTWO);
 
 	Collision::ColliderComponent* collider = new Collision::ColliderComponent(
 			collidingShape, new Actions::SuicideAction, myCategory, category);
